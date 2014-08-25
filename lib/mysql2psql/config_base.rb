4ef2ec958c3d5b5d98d3640d934b0deeaeb1321a
Jason Cox <jason@audioandpixels.com>
2014-08-25 16:24:23 +1000

    Update config_base.rb

--- lib/mysql2psql/config_base.rb
+++ lib/mysql2psql/config_base.rb
@@ -20,17 +20,17 @@ class Mysql2psql
       case token
       when /mysql/i
         key=token.sub( /^mysql/, '' )
-        value=config["mysql"][key]
+        value=config["mysql2psql"]["mysql"][key]
       when /pg/i
         key=token.sub( /^pg/, '' )
-        value=config["destination"]["postgres"][key]
+        value=config["mysql2psql"]["destination"]["postgres"][key]
       when /dest/i
         key=token.sub( /^dest/, '' )
-        value=config["destination"][key]
+        value=config["mysql2psql"]["destination"][key]
       when /only_tables/i
-        value=config["tables"] 
+        value=config["mysql2psql"]["tables"] 
       else
-        value=config[token]
+        value=config["mysql2psql"][token]
       end
       value.nil? ? ( must_be_defined ? (raise Mysql2psql::UninitializedValueError.new("no value and no default for #{name}")) : default ) : value
     end
